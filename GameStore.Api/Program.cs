36e77aaa1ef3589245732b0f43fbf201ad29a397
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-18 16:00:27 +0200

    After creating the games

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,25 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new(1,
+        "Street Fighter II",
+        "Fighting",
+        899.00M,
+        new DateOnly(1992, 7, 15)),
+    new(2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        245.00M,
+        new DateOnly(2010, 9, 20)),
+    new(3,
+        "Fifa 23",
+        "Sports",
+        1399.00M,
+        new DateOnly(2022, 9, 27))
+];
 
 app.MapGet("/", () => "Hello World!");
 
