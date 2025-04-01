a8dd7bb823d5240670e7efaa31c7d62cdcf2888d
Khuselo Nditha <khuselonditha@gmail.com>
2025-04-01 16:01:10 +0200

    After refactoring, removing in memory gamelist

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -8,25 +8,7 @@ namespace GameStore.Api.Endpoints;
 
 public static class GamesEndpoints {
     const string GetGameEndpointName = "GetGame";
-
-    private static readonly List<GameSummaryDto> games = [
-        new(1,
-            "Street Fighter II",
-            "Fighting",
-            899.00M,
-            new DateOnly(1992, 7, 15)),
-        new(2,
-            "Final Fantasy XIV",
-            "Roleplaying",
-            245.00M,
-            new DateOnly(2010, 9, 20)),
-        new(3,
-            "Fifa 23",
-            "Sports",
-            1399.00M,
-            new DateOnly(2022, 9, 27))
-    ];
-
+    
     public static RouteGroupBuilder MapGamesEndpoint(this WebApplication app) {
 
         var group = app.MapGroup("games")
