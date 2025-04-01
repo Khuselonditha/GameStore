b9e521501be4bec72873a344eb6b370653c79f34
Khuselo Nditha <khuselonditha@gmail.com>
2025-04-01 13:46:37 +0200

    After Adding the ToGAmeDetailsDto method

--- GameStore.Api/Mapping/GameMapping.cs
+++ GameStore.Api/Mapping/GameMapping.cs
@@ -18,13 +18,23 @@ public static class GameMapping
         
     } 
 
-
     public static GameSummaryDto ToSummaryDto(this Game game) 
     {   
         return new(
             game.Id,
             game.Name,
             game.Genre!.Name,
+            game.Price,
+            game.ReleaseDate
+        );
+    }
+
+    public static GameDetailsDto ToGameDetailsDto(this Game game)
+    {
+        return new(
+            game.Id,
+            game.Name,
+            game.GenreId,
             game.Price,
             game.ReleaseDate
         );
